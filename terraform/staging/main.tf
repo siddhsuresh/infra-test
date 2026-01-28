eaed1494f2025f77b2c0cffe7021f3716fa660cf
Siddharth Suresh <siddh.suresh@gmail.com>
2026-01-28 21:25:24 +0530

    Remove unused random provider and related resources; add SSM parameters for test values

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -6,10 +6,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
   }
 }
 
@@ -20,64 +16,26 @@ provider "aws" {
     tags = {
       Environment = var.environment
       ManagedBy   = "terraform"
-      Project     = "-e2e-test"
     }
   }
 }
 
-# Random suffix for unique bucket names
-resource "random_id" "bucket_suffix" {
-  byte_length = 4
+resource "aws_ssm_parameter" "test_param_1" {
+  name  = "/${var.environment}/test/param1"
+  type  = "String"
+  value = "test-value-1"
 }
 
-# S3 bucket
-resource "aws_s3_bucket" "test_bucket" {
-  bucket = "s-e2e-test-${var.environment}-${random_id.bucket_suffix.hex}"
-
-  tags = {
-    Name = "s E2E Test Bucket - ${var.environment}"
-  }
+resource "aws_ssm_parameter" "test_param_2" {
+  name  = "/${var.environment}/test/param2"
+  type  = "String"
+  value = "test-value-2"
 }
 
-resource "aws_s3_bucket_versioning" "test_bucket" {
-  bucket = aws_s3_bucket.test_bucket.id
-  versioning_configuration {
-    status = "Enabled"
-  }
+output "param1_name" {
+  value = aws_ssm_parameter.test_param_1.name
 }
 
-# Security group
-resource "aws_security_group" "test_sg" {
-  name        = "s-e2e-test-${var.environment}-${random_id.bucket_suffix.hex}"
-  description = "Security group for s E2E testing - ${var.environment}"
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "s E2E Test SG - ${var.environment}"
-  }
-}
-
-output "bucket_arn" {
-  value = aws_s3_bucket.test_bucket.arn
-}
-
-output "bucket_name" {
-  value = aws_s3_bucket.test_bucket.id
-}
-
-output "security_group_id" {
-  value = aws_security_group.test_sg.id
+output "param2_name" {
+  value = aws_ssm_parameter.test_param_2.name
 }
